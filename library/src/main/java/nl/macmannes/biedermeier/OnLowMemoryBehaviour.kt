0f0a3b890035530d9c1a0a8d41aaa0d7cf2a4203
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2018-02-21 12:41:58 +0100

    Added two behavior interfaces

---
+++ library/src/main/java/nl/macmannes/biedermeier/OnLowMemoryBehaviour.kt
@@ -0,0 +1,8 @@
+package nl.macmannes.biedermeier
+
+/**
+ * Created by paulvc on 21-02-18.
+ */
+interface OnLowMemoryBehaviour : ActivityBehaviour {
+    fun onLowMemory()
+}
---
+++ library/src/main/java/nl/macmannes/biedermeier/OnStopBehaviour.kt
@@ -0,0 +1,8 @@
+package nl.macmannes.biedermeier
+
+/**
+ * Created by paulvc on 21-02-18.
+ */
+interface OnStopBehaviour: ActivityBehaviour {
+    fun onStop()
+}
